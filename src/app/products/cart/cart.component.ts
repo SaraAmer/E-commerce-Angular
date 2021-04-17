372d9b8f07fbdca6feaa72af000fdcc4444588c5
Marwa Medhat <eng.marwamedhat2020@gmail.com>
2021-04-18 01:15:45 +0200

    edit

--- src/app/products/cart/cart.component.ts
+++ src/app/products/cart/cart.component.ts
@@ -5,76 +5,57 @@ import { AuthService } from 'src/app/services/auth.service';
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.scss']
+  styleUrls: ['./cart.component.scss'],
 })
 export class CartComponent implements OnInit {
-   cartProducts = [];
-   userCart=[];
-   total :number = 0;
-   name = localStorage.getItem('loginUser');
-   renderedValue: string ="0";
-value: number = 0;
-step: number = 1;
-min: number = 0;
-max: number ;
-symbol: string ="$";
-ariaLabelLess: string;
-ariaLabelMore: string;
-  constructor(private _loginService:AuthService) { }
+  cartProducts = [];
+  userCart = [];
+  total: number = 0;
+  name = localStorage.getItem('loginUser');
+  renderedValue: string = '0';
+  value: number = 0;
+  step: number = 1;
+  min: number = 0;
+  max: number = 10;
+  symbol: string = '$';
+  ariaLabelLess: string;
+  ariaLabelMore: string;
+  constructor(private _loginService: AuthService) {}
 
   ngOnInit(): void {
-      this._loginService.authUser.subscribe((data : string)=>{
-      this.name = data
-     
-     })
-   
-    for (let i = 0; i<localStorage.length; i++) {
-      
-      if(localStorage.key(i) != "loginUser")
-      {
-       
-         this.cartProducts[i] = JSON.parse(localStorage.getItem(localStorage.key(i)));  
-          // console.log("product"+JSON.parse(localStorage.getItem(localStorage.key(i).userName)))
-     
-      }
-      
-    }
-   
-    
-    for (let product of this.cartProducts)
-    {
-      if(product)
-      {
-        if(product.userName == this.name)
-        {
-          this.userCart.push(product)
-         
-        }
+    this._loginService.authUser.subscribe((data: string) => {
+      this.name = data;
+    });
 
+    for (let i = 0; i < localStorage.length; i++) {
+      if (localStorage.key(i) != 'loginUser') {
+        this.cartProducts[i] = JSON.parse(
+          localStorage.getItem(localStorage.key(i))
+        );
+        // console.log("product"+JSON.parse(localStorage.getItem(localStorage.key(i).userName)))
       }
-   
-    
+    }
+
+    for (let product of this.cartProducts) {
+      if (product) {
+        if (product.userName == this.name) {
+          this.userCart.push(product);
+        }
+      }
     }
 
     // console.log('products:' + (this.cartProducts[0].userName))
-     this.getotal();
-     console.log(this.total)
-
- 
+    this.getotal();
+    console.log(this.total);
   }
 
-  cartdelete(){
-
-  }
-  getotal() 
-  {
-    this.userCart.forEach((product) =>{
+  cartdelete() {}
+  getotal() {
+    this.userCart.forEach((product) => {
       console.log(product['productPrice']);
-      console.log(product['quantity'])
-     this.total += product['productPrice']*product['quantity'];
-   
-    })
-    
+      console.log(product['quantity']);
+      this.total += product['productPrice'] * product['quantity'];
+    });
   }
   toggleMore = () => {
     if (this.step + this.value <= this.max) {
@@ -82,12 +63,11 @@ ariaLabelMore: string;
       this.renderedValue = this.value.toString() + this.symbol;
     }
   };
-  
+
   toggleLess = () => {
     if (this.value - this.step >= this.min) {
       this.value = this.value - this.step;
       this.renderedValue = this.value.toString() + this.symbol;
     }
   };
-
 }

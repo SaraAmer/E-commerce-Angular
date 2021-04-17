0c6eb7eede76d037cf6f9cd1703d5ec06d97d08c
Your Name <you@example.com>
2021-04-17 22:36:20 +0200

    search,pagination and cart

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { AboutComponent } from './about/about.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
 import { LoginComponent } from './login/login.component';
 import { ProductsComponent } from './products/products.component';
+import { SearchComponent } from './products/search/search.component';
 
 const routes: Routes = [
  
 {path: "about" , component: AboutComponent},
 {path: "contact" , component : ContactUsComponent},
 { path: 'products', loadChildren: () => import('./products/products.module').then(m => m.ProductsModule) },
-{path:'login' , component:LoginComponent}
+{path:'login' , component:LoginComponent},
+{path:'search/:searchValue' , component :SearchComponent }
 ];
 
 @NgModule({
